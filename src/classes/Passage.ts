58a4a63c22a3d4d184520f05bdcf1df763f29087
Chris Tran <chris.tran@agilebits.com>
2024-03-28 14:43:11 -0500

    feat: adds createTransaction and verifyNonce methods to Passage class

--- src/classes/Passage.ts
+++ src/classes/Passage.ts
@@ -1,8 +1,9 @@
 import { PassageConfig } from '../types/PassageConfig';
 import { PassageError } from './PassageError';
-import { AppsApi, ResponseError } from '../generated';
+import { AppsApi, AuthenticateApi, Configuration, ResponseError, TransactionsApi } from '../generated';
 import apiConfiguration from '../utils/apiConfiguration';
 import { AppInfo } from '../models/AppInfo';
+import { TransactionArgs } from '../types/TransactionArgs';
 
 /**
  * Passage Class
@@ -10,18 +11,28 @@ import { AppInfo } from '../models/AppInfo';
 export default class Passage {
     public readonly apiKey?: string;
     public readonly appID: string;
+    private readonly configuration: Configuration;
+    private readonly appClient: AppsApi;
+    private readonly transactionClient: TransactionsApi;
+    private readonly authClient: AuthenticateApi;
 
     /**
      * Initialize a new Passage instance.
      * @param {PassageConfig} config The default config for Passage initialization
      */
-    public constructor({ appID, apiKey }: PassageConfig) {
-        if (!appID) {
+    public constructor(config: PassageConfig) {
+        if (!config.appID) {
             throw new PassageError('A Passage appID is required. Please include {appID: YOUR_APP_ID}.');
         }
 
-        this.appID = appID;
-        this.apiKey = apiKey;
+        this.appID = config.appID;
+        this.apiKey = config.apiKey;
+        this.configuration = apiConfiguration({
+            accessToken: this.apiKey,
+        });
+        this.appClient = new AppsApi(this.configuration);
+        this.transactionClient = new TransactionsApi(this.configuration);
+        this.authClient = new AuthenticateApi(this.configuration);
     }
 
     /**
@@ -31,11 +42,7 @@ export default class Passage {
      */
     public async getApp(): Promise<AppInfo> {
         try {
-            const configuration = apiConfiguration({
-                accessToken: this.apiKey,
-            });
-            const client = new AppsApi(configuration);
-            const response = await client.getApp({
+            const response = await this.appClient.getApp({
                 appId: this.appID,
             });
 
@@ -44,11 +51,54 @@ export default class Passage {
                 id,
                 name,
                 authOrigin: auth_origin,
-             };
+            };
 
             return appInfo;
         } catch (err) {
-            throw new PassageError('Could not fetch app.', err as ResponseError);
+            throw new PassageError('Could not fetch app', err as ResponseError);
+        }
+    }
+
+    /**
+     * Create a transaction to start a user's registration or authentication process
+     *
+     * @param {TransactionArgs} args The required values to create a transaction
+     * @return {Promise<string>} The transaction ID
+     */
+    public async createTransaction(args: TransactionArgs): Promise<string> {
+        try {
+            const response = await this.transactionClient.createTransaction({
+                appId: this.appID,
+                createTransactionRequest: {
+                    external_id: args.externalId,
+                    passkey_display_name: args.passkeyDisplayName,
+                },
+            });
+
+            return response.transaction_id;
+        } catch (err) {
+            throw new PassageError('Could not create transaction', err as ResponseError);
+        }
+    }
+
+    /**
+     * Verify the nonce received from a WebAuthn registration or authentication ceremony
+     *
+     * @param {string} nonce The nonce to verify
+     * @return {Promise<boolean>} Whether the nonce was verified
+     */
+    public async verifyNonce(nonce: string): Promise<boolean> {
+        try {
+            await this.authClient.authenticateVerifyNonce({
+                appId: this.appID,
+                body: {
+                    nonce,
+                },
+            });
+
+            return true;
+        } catch (err) {
+            throw new PassageError('Could not verify nonce', err as ResponseError);
         }
     }
 }
--- tests/Passage.test.ts
+++ tests/Passage.test.ts
@@ -4,6 +4,8 @@ import { PassageError } from '../src/classes/PassageError';
 require('dotenv').config();
 
 describe('Passage', () => {
+    const expectedAppID = process.env.TEST_APP_ID;
+
     describe('constructor', () => {
         it.each(['', undefined, null])('should throw an error if appID is %s', (appID) => {
             expect(() => {
@@ -13,7 +15,6 @@ describe('Passage', () => {
     });
 
     describe('getApp', () => {
-        const expectedAppID = process.env.TEST_APP_ID;
         let passage: Passage;
 
         beforeEach(() => {
@@ -30,6 +31,48 @@ describe('Passage', () => {
                 name: expect.any(String),
                 authOrigin: expect.any(String),
             });
+        });
+
+        it('should throw an error if the app does not exist', async () => {
+            const passage = new Passage({
+                appID: 'invalid',
+                apiKey: process.env.TEST_API_KEY,
+            });
+            await expect(passage.getApp()).rejects.toThrow(PassageError);
+        });
+    });
+
+    describe('createTransaction', () => {
+        let passage: Passage;
+
+        beforeEach(() => {
+            passage = new Passage({
+                appID: expectedAppID,
+                apiKey: process.env.TEST_API_KEY,
+            });
+        });
+
+        it('should return the transaction ID', async () => {
+            const transactionID = await passage.createTransaction({
+                externalId: 'test',
+                passkeyDisplayName: 'test',
+            });
+            expect(transactionID).toEqual(expect.any(String));
+        });
+    });
+
+    describe('verifyNonce', () => {
+        let passage: Passage;
+
+        beforeEach(() => {
+            passage = new Passage({
+                appID: expectedAppID,
+                apiKey: process.env.TEST_API_KEY,
+            });
+        });
+
+        it('should throw an error if the nonce is invalid', async () => {
+            await expect(passage.verifyNonce('invalid')).rejects.toThrow(PassageError);
         });
     });
 });

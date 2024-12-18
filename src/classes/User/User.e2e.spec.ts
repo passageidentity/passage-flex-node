82e1b5562c211c0c7bdbaa50fb851bd3fe5039e3
Chris Tran <chris.tran@agilebits.com>
2024-12-17 19:33:02 -0600

    test: fixes test assertion from rebase

--- src/classes/User/User.e2e.spec.ts
+++ src/classes/User/User.e2e.spec.ts
@@ -36,7 +36,7 @@ describe('User e2e', () => {
         });
 
         it('should throw an error if the user does not exist', async () => {
-            await expect(passage.user.get('invalid')).rejects.toThrow('Could not find user with that external ID');
+            await expect(passage.user.get('invalid')).rejects.toThrow('User not found.');
         });
     });
 
@@ -61,9 +61,7 @@ describe('User e2e', () => {
         });
 
         it('should throw an error if the user does not exist', async () => {
-            await expect(passage.user.listDevices('invalid')).rejects.toThrow(
-                'Could not find user with that external ID',
-            );
+            await expect(passage.user.listDevices('invalid')).rejects.toThrow('User not found.');
         });
     });
 });

5df068e3285ae18688c11ffc6316db0da8b2e2ed
Tamara Deshong <tamara.deshong@agilebits.com>
2025-01-06 10:48:33 -0500

    style: format

--- eslint.config.js
+++ eslint.config.js
@@ -4,41 +4,41 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: {
-          allowDefaultProject: ['eslint.config.js'],
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: {
+                    allowDefaultProject: ['eslint.config.js'],
+                },
+                tsconfigRootDir: import.meta.dirname,
+            },
         },
-        tsconfigRootDir: import.meta.dirname,
-      },
     },
-  },
-  {
-    files: ['**/*.ts'],
-  },
-  {
-    ignores: ['src/generated'],
-  },
-  {
-    rules: {
-      '@typescript-eslint/switch-exhaustiveness-check': [
-        'error',
-        {
-          considerDefaultExhaustiveForUnions: true,
-        },
-      ],
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          varsIgnorePattern: '^_',
-          argsIgnorePattern: '^_',
-        },
-      ],
+    {
+        files: ['**/*.ts'],
     },
-  },
+    {
+        ignores: ['src/generated'],
+    },
+    {
+        rules: {
+            '@typescript-eslint/switch-exhaustiveness-check': [
+                'error',
+                {
+                    considerDefaultExhaustiveForUnions: true,
+                },
+            ],
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    varsIgnorePattern: '^_',
+                    argsIgnorePattern: '^_',
+                },
+            ],
+        },
+    },
 );
